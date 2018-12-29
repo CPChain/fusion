9aed78894d4cecd30bb5d9a5765e7811ea9a1af1
wujj <wujj@cpchain.io>
2018-12-29 16:48:12 +0800

    add some commit

--- create_key_test.py
+++ create_key_test.py
@@ -8,24 +8,26 @@ from cpc_fusion import Web3
 from cpc_fusion.middleware import geth_poa_middleware
 from cpc_fusion.cpc_keyfile import decode_keyfile_json
 
-
-def main():
+# def tx1():
+#     web3 = Web3(Web3.HTTPProvider('http://192.168.50.251:8501'))
+#     print("http://192.168.50.251:8545")
+#     print(web3.cpc.blockNumber)
+#     # inject the poa compatibility middleware to the innermost layer
+#     web3.middleware_stack.inject(geth_poa_middleware, layer=0)
+#     print('to  ', web3.cpc.getBalance(web3.toChecksumAddress('c05302acebd0730e3a18a058d7d1cb1204c4a092')))
+#     print('from', web3.cpc.getBalance(web3.toChecksumAddress('e94b7b6c5a0e526a4d97f9768ad6097bde25c62a')))
+#
+#     print(web3.personal.sendTransaction({'to': web3.toChecksumAddress('c05302acebd0730e3a18a058d7d1cb1204c4a092'),
+#                                           'from': web3.toChecksumAddress('e94b7b6c5a0e526a4d97f9768ad6097bde25c62a'),
+#                                           'value': 321},
+#                                          'password').hex())
+#     print('to  ', web3.cpc.getBalance(web3.toChecksumAddress('c05302acebd0730e3a18a058d7d1cb1204c4a092')))
+#
+#     print('from', web3.cpc.getBalance(web3.toChecksumAddress('e94b7b6c5a0e526a4d97f9768ad6097bde25c62a')))
+#
+#
+def tx2():
     web3 = Web3(Web3.HTTPProvider('http://192.168.50.251:8501'))
-    print("http://192.168.50.251:8545")
-    print(web3.cpc.blockNumber)
-    # inject the poa compatibility middleware to the innermost layer
-    web3.middleware_stack.inject(geth_poa_middleware, layer=0)
-    print('to  ', web3.cpc.getBalance(web3.toChecksumAddress('c05302acebd0730e3a18a058d7d1cb1204c4a092')))
-    print('from', web3.cpc.getBalance(web3.toChecksumAddress('e94b7b6c5a0e526a4d97f9768ad6097bde25c62a')))
-
-    print((web3.personal.sendTransaction({'to': web3.toChecksumAddress('c05302acebd0730e3a18a058d7d1cb1204c4a092'),
-                                          'from': web3.toChecksumAddress('e94b7b6c5a0e526a4d97f9768ad6097bde25c62a'),
-                                          'value': 123},
-                                         'password')).hex())
-    print('to  ', web3.cpc.getBalance(web3.toChecksumAddress('c05302acebd0730e3a18a058d7d1cb1204c4a092')))
-
-    print('from', web3.cpc.getBalance(web3.toChecksumAddress('e94b7b6c5a0e526a4d97f9768ad6097bde25c62a')))
-    print(web3.cpc.getTransaction('0xee7b935bec5bae9ef2bd7208a4c134cebd18da15cc033a3b24549bfe2c171397'))
     with open('./keys/key1') as keyfile:
         encrypted_key = keyfile.read()
     # print(web3.cpc.getBalance(web3.cpc.accounts))
@@ -38,7 +40,6 @@ def main():
     print("private_key_for_senders_account:")
     print(private_key_for_senders_account)
 
-    web3 = Web3(Web3.HTTPProvider('http://192.168.50.251:8501'))
     nonce = web3.cpc.getTransactionCount(web3.cpc.coinbase)
     addr = web3.toChecksumAddress('0xc05302acebd0730e3a18a058d7d1cb1204c4a092')
     fromA = web3.toChecksumAddress('0xe94b7b6c5a0e526a4d97f9768ad6097bde25c62a')
@@ -55,7 +56,7 @@ def main():
         value=123,
         data=b'',
         extra=b'',
-
+        chainId=41,
     )
     ddd['from'] =  fromA
     signed_txn = web3.cpc.account.signTransaction(ddd,
@@ -66,8 +67,11 @@ def main():
 
     print("sendRawTransaction:")
     print(web3.toHex(signed_txn.rawTransaction))
-    web3.cpc.sendRawTransaction(signed_txn.rawTransaction)
+    print(web3.cpc.sendRawTransaction(signed_txn.rawTransaction))
 
+def main():
+    # tx1()
+    tx2()
 
 if __name__ == '__main__':
     main()
