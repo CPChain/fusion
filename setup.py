4b4add97140dc07c89f19c27cebd412caad63cd1
zgljl2012 <2693491512@qq.com>
2021-07-28 15:57:44 +0800

    upgrade cpc-abi

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
         "toolz>=0.9.0,<1.0.0;implementation_name=='pypy'",
         "cytoolz>=0.9.0,<1.0.0;implementation_name=='cpython'",
         # "eth-abi==1.3.0",
-        "cpc-abi>=1.3.3",
+        "cpc-abi>=1.3.4",
         "eth-account>=0.2.1,<0.4.0",
         "eth-utils>=1.2.0,<2.0.0",
         "hexbytes>=0.1.0,<1.0.0",
