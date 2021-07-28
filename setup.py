9ff06c8e198f238ae49ed2bdc34f32a8bd131c65
zgljl2012 <2693491512@qq.com>
2021-07-28 15:57:48 +0800

    Bump version: 0.1.11 → 0.1.12

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ extras_require['dev'] = (
 setup(
     name='cpc-fusion',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.1.11',
+    version='0.1.12',
     description="""CPC Fusion""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam , CPChain',
