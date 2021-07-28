f41aca0a860a12fb2299c1d2fba2eeb4775c37f4
zgljl2012 <2693491512@qq.com>
2021-07-28 15:32:04 +0800

    Bump version: 0.1.7 → 0.1.8

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ extras_require['dev'] = (
 setup(
     name='cpc-fusion',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.1.7',
+    version='0.1.8',
     description="""CPC Fusion""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam , CPChain',
@@ -71,7 +71,8 @@ setup(
     install_requires=[
         "toolz>=0.9.0,<1.0.0;implementation_name=='pypy'",
         "cytoolz>=0.9.0,<1.0.0;implementation_name=='cpython'",
-        "eth-abi==1.3.0",
+        # "eth-abi==1.3.0",
+        "cpc-abi>=1.3.3",
         "eth-account>=0.2.1,<0.4.0",
         "eth-utils>=1.2.0,<2.0.0",
         "hexbytes>=0.1.0,<1.0.0",
